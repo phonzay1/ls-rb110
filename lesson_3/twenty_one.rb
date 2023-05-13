9f4d3d580e6824dcc4c775d45d16148a87d5b547
Ping Honzay <phonzay1@gmail.com>
2023-05-13 17:17:58 +0000

    Twenty-One Game

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+MAX_HAND_VALUE = 41
+DEALER_HIT_UNTIL = 37
+
 SUITS = %w(H D S C)
 VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
@@ -11,8 +14,8 @@ def joinand(arr)
   case arr.size
   when 0 then ""
   when 1 then arr[0].to_s
-  when 2 then arr[0].to_s + 'and' + arr[1].to_s
-  else "#{arr[0..-2].join(', ')} and" + arr[-1].to_s
+  when 2 then arr[0].to_s + ' and ' + arr[1].to_s
+  else "#{arr[0..-2].join(', ')}, and " + arr[-1].to_s
   end
 end
 
@@ -38,7 +41,7 @@ def calculate_hand(hand)
     end
   end
 
-  hand.map { |card| card[1] }.count('A').times { |_| sum -= 10 if sum > 21 }
+  hand.map { |card| card[1] }.count('A').times { |_| sum -= 10 if sum > MAX_HAND_VALUE }
   sum
 end
 
@@ -49,7 +52,7 @@ def player_turn(hand, deck)
     if answer.start_with?('h')
       hand << deck.pop
       values = hand.map { |card| display_value(card) }
-      prompt "You have #{joinand(values)} (total: #{calculate_hand(hand)})"
+      prompt "You have: #{joinand(values)} (total: #{calculate_hand(hand)})"
       break if busted?(hand)
     elsif answer.start_with?('s') || busted?(hand)
       break
@@ -61,7 +64,7 @@ end
 
 def dealer_turn(hand, deck)
   loop do
-    break if calculate_hand(hand) >= 17 || busted?(hand)
+    break if calculate_hand(hand) >= DEALER_HIT_UNTIL || busted?(hand)
     prompt "Dealer hit!"
     hand << deck.pop
     values = hand.map { |card| display_value(card) }
@@ -70,25 +73,20 @@ def dealer_turn(hand, deck)
 end
 
 def busted?(hand)
-  calculate_hand(hand) > 21
+  calculate_hand(hand) > MAX_HAND_VALUE
 end
 
-def return_winner(player_hand, dealer_hand)
-  if calculate_hand(player_hand) > calculate_hand(dealer_hand)
+def return_winner(player_total, dealer_total)
+  if player_total > dealer_total
     "You"
-  elsif calculate_hand(dealer_hand) > calculate_hand(player_hand)
+  elsif dealer_total > player_total
     "Dealer"
   else
     "Tie"
   end
 end
 
-def display_results(winner, player_hand, dealer_hand)
-  player_hand_values = player_hand.map { |card| display_value(card) }
-  dealer_hand_values = dealer_hand.map { |card| display_value(card) }
-  prompt "Your ending hand was: #{joinand(player_hand_values)} (total: " \
-         "#{calculate_hand(player_hand)}). Dealer's ending hand was: " \
-         "#{joinand(dealer_hand_values)} (total: #{calculate_hand(dealer_hand)})."
+def display_winner(winner)
   if winner == "You"
     prompt "Congratulations, you win!"
   elsif winner == "Dealer"
@@ -98,25 +96,69 @@ def display_results(winner, player_hand, dealer_hand)
   end
 end
 
+def display_final_hands(player_hand, dealer_hand, player_total, dealer_total,
+                       player_wins, dealer_wins)
+  player_hand_values = player_hand.map { |card| display_value(card) }
+  dealer_hand_values = dealer_hand.map { |card| display_value(card) }
+  prompt "Your ending hand was: #{joinand(player_hand_values)} (total: " \
+         "#{player_total}). Dealer's ending hand was: " \
+         "#{joinand(dealer_hand_values)} (total: #{dealer_total})."
+  prompt "You have won #{player_wins} times. Dealer has won #{dealer_wins} " \
+         "times."
+end
+
+def detect_overall_winner(player_wins, dealer_wins)
+  if player_wins >= 5
+    'Player'
+  elsif dealer_wins >= 5
+    'Dealer'
+  end
+end
+
+def display_overall_winner(overall_winner)
+  case overall_winner
+  when 'Player' then prompt "You have won 5 rounds. You're the grand champion!"
+  when 'Dealer' then prompt "Dealer has won 5 rounds. They win the game!"
+  end
+end
+
 def play_again?
   prompt "Would you like to play again? (enter 'y' for yes, 'n' for no)"
   gets.chomp.downcase.start_with?('y')
 end
 
+# Start of game play
+prompt "Welcome to Twenty-One! You'll be playing against the Dealer (the " \
+       "computer). First player to win 5 rounds is the overall winner."
+
+player_wins = 0
+dealer_wins = 0
+
 loop do
   deck = SUITS.product(VALUES).shuffle
   player_hand = deck.pop(2)
   dealer_hand = deck.pop(2)
+  player_total = calculate_hand(player_hand)
+  dealer_total = calculate_hand(dealer_hand)
 
   prompt "Dealer has: #{display_value(dealer_hand.first)} and unknown card"
   prompt "You have: #{display_value(player_hand.first)} and " \
-         "#{display_value(player_hand.last)} (total: #{calculate_hand(player_hand)})"
+         "#{display_value(player_hand.last)} (total: #{player_total})"
 
   player_turn(player_hand, deck)
+  player_total = calculate_hand(player_hand)
 
   if busted?(player_hand)
     prompt "You busted! Dealer wins."
-    if play_again?
+    dealer_wins += 1
+    display_final_hands(player_hand, dealer_hand, player_total, dealer_total,
+                       player_wins, dealer_wins)
+    overall_winner = detect_overall_winner(player_wins, dealer_wins)
+
+    if overall_winner
+      display_overall_winner(overall_winner)
+      break
+    elsif play_again?
       system 'clear'
       next
     else
@@ -127,10 +169,19 @@ loop do
   end
 
   dealer_turn(dealer_hand, deck)
+  dealer_total = calculate_hand(dealer_hand)
 
   if busted?(dealer_hand)
     prompt "Dealer busted - you win!"
-    if play_again?
+    player_wins += 1
+    display_final_hands(player_hand, dealer_hand, player_total, dealer_total,
+                       player_wins, dealer_wins)
+    overall_winner = detect_overall_winner(player_wins, dealer_wins)
+
+    if overall_winner
+      display_overall_winner(overall_winner)
+      break
+    elsif play_again?
       system 'clear'
       next
     else
@@ -140,8 +191,22 @@ loop do
     prompt "Dealer chose to stay."
   end
 
-  winner = return_winner(player_hand, dealer_hand)
-  display_results(winner, player_hand, dealer_hand)
+  winner = return_winner(player_total, dealer_total)
+
+  case winner
+  when 'You' then player_wins += 1
+  when 'Dealer' then dealer_wins += 1
+  end
+
+  display_winner(winner)
+  display_final_hands(player_hand, dealer_hand, player_total, dealer_total,
+                     player_wins, dealer_wins)
+
+  overall_winner = detect_overall_winner(player_wins, dealer_wins)
+  if overall_winner
+    display_overall_winner(overall_winner)
+    break
+  end
 
   break unless play_again?
   system 'clear'
